da976d7eef82a56c81b1181124cdf0c1f4551872
jmdecerio <jmdecerio@gmail.com>
2026-01-01 19:05:31 +0100

    Web search con Tavily search and extract using lmstudio with gpt-oss-20b model, list of tools lo load configurable

--- src/main/java/com/jmdecerio/spring_ai_mcp_client/controllers/ChatController.java
+++ src/main/java/com/jmdecerio/spring_ai_mcp_client/controllers/ChatController.java
@@ -5,14 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.tool.ToolCallback;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Slf4j
 @RestController
